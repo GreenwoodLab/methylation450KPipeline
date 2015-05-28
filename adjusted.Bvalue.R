42346b5a775d6576ad3cc03c26aeb21f854adeba
adminGreenwoodLab <greg.voisin@ladydavis.com>
2015-05-28 14:46:47 -0400

    adjusted.Bvalue
    Ajuste B value for value near to 0 and 1

---
+++ adjusted.Bvalue.R
@@ -0,0 +1,4 @@
+adjusted.Bvalue <- function(matrixValue){for ( i in 1:ncol(matrixValue)){ matrixValue[,i] <- (matrixValue[,i]*(1000-1)+0.5)/1000
+                     return(matrixValue)
+                     }
+                   }#end of adjusted.Bvalue
