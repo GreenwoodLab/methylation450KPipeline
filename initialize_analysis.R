413abb3701513e7184d4de6e4689bbffffd5d16c
adminGreenwoodLab <greg.voisin@ladydavis.com>
2015-05-28 15:25:37 -0400

    initialize_analysis
    create the folder

---
+++ initialize_analysis.R
@@ -0,0 +1,14 @@
+
+source("~/share/greenwood.group/Rscripts/methylation_R_scripts/fun_initialize_analysis.R")
+
+print(paste("Beginning of the analysis :\n", Sys.time(),sep= ""))
+ 
+print("..Initialize the analysis: create the folder")
+
+    #get the arguments from batch command line
+    args <- commandArgs(trailingOnly = TRUE)
+
+    #process the process of IdatData before inspection of QC
+    initialize_analysis(args[1])
+
+print("..End of the creation of the Folder")
