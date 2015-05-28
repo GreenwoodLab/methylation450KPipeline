7198984697092e90d15de41686ba365ec04643b2
adminGreenwoodLab <greg.voisin@ladydavis.com>
2015-05-28 14:56:32 -0400

    copyIDAT_sampleSheet
    copy IDAT and sampleSheet from source folder to PROJET folder

---
+++ copyIDAT_sampleSheet.R
@@ -0,0 +1,16 @@
+#script for methylation pipeline analysis
+#february 2014
+#voisin greg - Greenwood lab- ldi
+
+################################################################################
+source("fun_copyIDAT_sampleSheet.R")
+
+print(paste("Beginning of copy of IDAT file and samplesheet :\n", Sys.time(),sep= ""))
+
+    #get the arguments from batch command line
+    args <- commandArgs(trailingOnly = TRUE)
+
+    #process the copy of Idat files 
+    copyIDAT_sampleSheet(args[1], args[2])
+
+print("..The idat files and csv are copied in IDAT folder of the analysis")
