9b1095e8e15b7923786a0cac5f64fc73cb5a9689
adminGreenwoodLab <greg.voisin@ladydavis.com>
2015-06-15 12:20:19 -0400

    Create processingIdatData_before_QC.R

---
+++ processingIdatData_before_QC.R
@@ -0,0 +1,11 @@
+source("methylation_R_scripts/fun_processingIdatData_before_QC.R")
+
+print(paste("Beginning the Idat data process of :"\n, Sys.time(),sep= ""))
+    
+    #get the arguments from batch command line
+    args <- commandArgs(trailingOnly = TRUE)
+
+    #process the process of IdatData before inspection of QC
+    processingIdatData_before_QC(args[1], args[2])
+    
+print("..The Idat file have been processed")
