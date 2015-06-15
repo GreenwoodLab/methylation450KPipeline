459a06aeac82ba88b04dabb0bda259343c25cd87
adminGreenwoodLab <greg.voisin@ladydavis.com>
2015-06-15 10:09:17 -0400

    Update adjusted.Bvalue.R

--- adjusted.Bvalue.R
+++ adjusted.Bvalue.R
@@ -1,5 +1,4 @@
 adjusted.Bvalue <- function(matrixValue){
                     for ( i in 1:ncol(matrixValue)){ matrixValue[,i] <- (matrixValue[,i]*(1000-1)+0.5)/1000
                      return(matrixValue)
-                     }
-                    }#end of adjusted.Bvalue
+  }}#end of adjusted.Bvalue
