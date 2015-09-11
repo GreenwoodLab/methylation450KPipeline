f1010d9151e99f28cefb573bf235819b97f52757
Statistical Genetics at the Lady Davis Institute <voisingreg@yahoo.fr>
2015-09-11 13:29:44 -0400

    Update fun_copyAnnotation.R
    add  Environnement variable PIPELINE_450K

--- fun_copyAnnotation.R
+++ fun_copyAnnotation.R
@@ -1,7 +1,7 @@
 #Copy all annotation data
 #  input: path1: folder analysis
 copyAnnotation <- function(path1, path0){
-  path0 = "~/ANNOTATION_FOR450K_PIPELINE"
+  path0 = paste(Sys.getenv("PIPELINE_450K"),"/ANNOTATION_FOR450K_PIPELINE")
   path1 = paste(path1, "ANNOTATION_DATA", sep = "/")
   system(paste("cp",paste(path0,"*.*", sep = "/"), path1 , sep = " "))
 }
