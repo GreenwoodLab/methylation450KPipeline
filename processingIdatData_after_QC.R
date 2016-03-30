74913b7a1398cd1eb61e988e902eba4f14be85ab
Kathleen Klein Oros <kkleinoros@users.noreply.github.com>
2016-03-30 16:40:30 -0400

    removed base in read.450k.exp
    base is already included in Basename

--- processingIdatData_after_QC.R
+++ processingIdatData_after_QC.R
@@ -22,11 +22,11 @@ processingIdatData_after_QC <- function(path, server, cutoff=23){
       exclusion_pos  = append(exclusion_pos, grep(exclusionSampleVector[i], targets$Basename))
     }
     targets_after_QC <- targets[-exclusion_pos,]
-    RGset_after_QC   <- read.450k.exp(base = paste(path,"IDAT", sep= "/"), targets = targets_after_QC)
+    RGset_after_QC   <- read.450k.exp(targets = targets_after_QC)
 
     #write the Fun_Norm data in normalize data folder
-    FunNorm_data_bckdyeAdj_after_QC       <- preprocessFunnorm(RGset_after_QC,sex = NULL,, verbose=T)           
-	FunNorm_data_NobckdyeAdj_after_QC     <- preprocessFunnorm(RGset_after_QC,sex = NULL, verbose=T, bgCorr = FALSE, dyeCorr = FALSE)          
+  FunNorm_data_bckdyeAdj_after_QC       <- preprocessFunnorm(RGset_after_QC,sex = NULL,, verbose=T)           
+  FunNorm_data_NobckdyeAdj_after_QC     <- preprocessFunnorm(RGset_after_QC,sex = NULL, verbose=T, bgCorr = FALSE, dyeCorr = FALSE)          
   FunNorm_Bvalue_bckdyeAdj_after_QC     <- getBeta(FunNorm_data_bckdyeAdj_after_QC)                     
   FunNorm_Bvalue_NobckdyeAdj_after_QC   <- getBeta(FunNorm_data_NobckdyeAdj_after_QC)                         
    save(FunNorm_Bvalue_bckdyeAdj_after_QC, file = paste(path,"FUN_NORM_DATA/FunNorm_Bvalue_bckdyeAdj_after_QC.RData", sep = "/"))
